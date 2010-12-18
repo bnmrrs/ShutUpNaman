d111e190d449f6b268357009df388adeaeca1b14
Jamie Murai <murai.jamie@gmail.com>
2010-12-18 13:17:06 -0500

    Removed more unnecessary files

--- config/config.rb
+++
@@ -1,8 +0,0 @@
-configure do 
-  file_name = File.join(File.dirname(__FILE__), "..", "config", "mongoid.yml")
-  @settings = YAML.load(ERB.new(File.new(file_name).read).result)
-
-  Mongoid.configure do |config|
-    config.from_hash(@settings[ENV['RACK_ENV']])
-  end
-end
--- lib/post.rb
+++
@@ -1,9 +0,0 @@
-class Post
-  include Mongoid::Document
-   field :id
-   field :name
-   field :date
-   field :type
-   field :post
-   field :summary
-end
